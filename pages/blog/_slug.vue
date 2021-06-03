c40d3758016ff85e42fb48bd1fa3c02bcd49729b
Whale Mo <ncwhale@gmail.com>
2021-06-03 12:52:40 +0900

    Add content slug for blog articles.

---
+++ pages/blog/_slug.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+article
+  nuxt-content(:document="article")
+  pre {{ article }}
+</template>
+
+<script>
+export default {
+  async asyncData({ $content, params }) {
+    // fetch our article here
+    const article = await $content("blog", params.slug).fetch();
+
+    return { article };
+  },
+};
+</script>
